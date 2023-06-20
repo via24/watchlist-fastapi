d834951282c24e22b8ab69ea4884b1a718076244
Ray <58332748+via24@users.noreply.github.com>
2023-06-20 13:01:07 +0800

    required

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     env: USERNAME, EMAIL, PWD, DB_URL, SECRET_KEY, HOST, PORT
     """
     host = os.getenv("HOST", "0.0.0.0")
-    port = os.getenv("PORT", 80)
+    port = os.getenv("PORT", 8888)
     uvicorn.run(app, host=host, port=port)
